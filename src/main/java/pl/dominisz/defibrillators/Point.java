b5638e85c4c6a620e928a347a84eaf2fde450ebf
dominisz <dominisz@gmail.com>
2018-03-28 17:54:00 +0200

    Change field visibility for Defibrillator

--- src/main/java/pl/dominisz/defibrillators/Point.java
+++ src/main/java/pl/dominisz/defibrillators/Point.java
@@ -6,17 +6,17 @@ package pl.dominisz.defibrillators;
  */
 public class Point {
 
-    final double EARTH_RADIUS = 6371.0;
+    private final double EARTH_RADIUS = 6371.0;
 
-    double longitude;
-    double latitude;
+    private double longitude;
+    private double latitude;
 
     public Point(double longitude, double latitude) {
         this.longitude = longitude;
         this.latitude = latitude;
     }
 
-    double distanceTo(Point B) {
+    public double distanceTo(Point B) {
         double x = (B.longitude - longitude) * Math.cos((latitude + B.latitude) / 2);
         double y = B.latitude - latitude;
         double d = Math.sqrt(x * x + y * y) * EARTH_RADIUS;
